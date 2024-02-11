311eb85708e83732cf9b20ef13f1ae0aa337f91d
Bruno Luiz Silva <contact@brunoluiz.net>
2024-02-11 22:49:54 +0000

    chore: openapi using references

--- services/radars/gen.go
+++ services/radars/gen.go
@@ -1,5 +1,5 @@
 package radars
 
-//go:generate oapi-codegen -config=./oapi-codegen.yaml openapi.yaml
+//go:generate oapi-codegen -config=./oapi-codegen.yaml openapi/main.yaml
 //go:generate sqlc generate --file ./sqlc.yaml
 //go:generate killall gopls # otherwise vim will not detect changes properly
