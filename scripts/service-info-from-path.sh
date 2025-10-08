940ad870388a7f31df45cfbd5e42f53204d56a0c
Bruno Luiz Silva <contact@brunoluiz.net>
2025-10-08 22:26:35 +0100

    chore: monogo

---
+++ scripts/service-info-from-path.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FULL_PATH="$1"
+PATH_WITHOUT_PREFIX="${FULL_PATH#services/}"
+
+SERVICE_NAME=$(echo "$PATH_WITHOUT_PREFIX" | cut -d'/' -f1)
+CMD_NAME=$(echo "$PATH_WITHOUT_PREFIX" | cut -d'/' -f3)
+
+# Export the variables
+export service="$SERVICE_NAME"
+export cmd="$CMD_NAME"
+
+echo "service=$service"
+echo "cmd=$cmd"
